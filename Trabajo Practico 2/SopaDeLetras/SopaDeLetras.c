dd44fc55fef62a6bd125b15a8359ab7aa9a5094f
Jonas Pollnow <jpjonas35@gmail.com>
2017-10-30 20:54:58 -0300

    asd

--- Trabajo Practico 2/SopaDeLetras/SopaDeLetras.c
+++ Trabajo Practico 2/SopaDeLetras/SopaDeLetras.c
@@ -1,10 +1,3 @@
-/* 
- * File:   SopaDeLetras.c
- * Author: Jonas Pollnow
- *
- * Created on October 30, 2017, 9:23 AM
- */
-
 #include "SopaDeLetras.h"
 
 /*
@@ -18,7 +11,7 @@ int main()
   int m=0;
   while(m!=1 || m!=2 || m!=3)
   {
-    //system("clear");
+    system("clear");
     printf("Sopa de Letras\n");
     printf("1 - Jugar\n");
     printf("2 - Testear Funciones\n");
@@ -33,7 +26,7 @@ int main()
         //testearFunciones();
         break;
       case 3:
-        //system("clear");
+        system("clear");
         return 0;
     }
   }
@@ -47,53 +40,92 @@ Retorno representa como el el programa fue finalizado
 */
 void jugar()
 {
-  //Variables
-  int i=0, j=0, tamanioUniverso=0, numeroFilas=0, numeroColumnas=0;
+    //Variables
+    int i=0, tamanioUniverso=0, numeroFilas=0, numeroColumnas=0;
 
-  //Definición del Universo
-  //system("clear");
-  printf("Universo\n");
-  printf("\n¿Cual es el tamaño del universo de palabras? ");
-  scanf("%d",&tamanioUniverso);
+    //Definición del Universo
+    system("clear");
+    printf("Universo\n");
+    printf("\n¿Cual es el tamaño del universo de palabras? ");
+    scanf("%d",&tamanioUniverso);
   
-  char Universo[tamanioUniverso][100];
-  for(i=0;i<tamanioUniverso;i++)
-  {
-    printf("\nIngrese la %d palabra del universo: ", i+1);
-    scanf("%s",Universo[i]);
-  }
+    char Universo[tamanioUniverso][MAXPALABRA];
+    for(i=0; i<tamanioUniverso; i++)
+    {
+        printf("\nIngrese la %d palabra del universo: ", i+1);
+        scanf("%s",Universo[i]);
+    }
 
-  //Definición de la Sopa de Letras
-  //system("clear");
-  printf("Sopa de Letras\n");
-  printf("\n¿Cual es el número de filas de la sopa de letras? ");
-  scanf("%d",&numeroFilas);
-  printf("\n¿Cual es el número de columnas de la sopa de letras? ");
-  scanf("%d",&numeroColumnas);
+    //Definición de la Sopa de Letras
+    system("clear");
+    printf("Sopa de Letras\n");
+    printf("\n¿Cual es el número de filas de la sopa de letras? ");
+    scanf("%d",&numeroFilas);
+    printf("\n¿Cual es el número de columnas de la sopa de letras? ");
+    scanf("%d",&numeroColumnas);
 
-  char SopaLetras[numeroFilas][numeroColumnas];
-  printf("\nIngrese la Sopa de Letras:\n");
+    char SopaLetras[numeroFilas][numeroColumnas];
+    printf("\nIngrese la Sopa de Letras:\n");
   
-  for(i=0; i<numeroFilas; i++)
-  {
-    printf("\nIngresse la primera fila de la Sopa de Letras: ");
-    scanf("%s",SopaLetras[i]);
-  }
+    for(i=0; i<numeroFilas; i++)
+    {
+        printf("\nIngresse la primera fila de la Sopa de Letras: ");
+        scanf("%s",SopaLetras[i]);
+    }
+    imprimeSopaLetras(numeroFilas,numeroColumnas,SopaLetras);
  
-  
-  
-    printf("Ingresse la primera fila de la Sopa de Letras: ");
-  //for(i = 0; i < m; i++){
-  //   for(k = 0; k < m; k++){
-  //     printf("%2d", array[i][k]);    // print array.
-  //   }
-  //   printf("\n");
-  //} 
-
-
+    buscarPalabras(tamanioUniverso,numeroFilas,numeroColumnas,Universo,SopaLetras);
 }
 
+/*
+Representamos el tamaño del universo,
+jugar : void
+Retorno representa como el el programa fue finalizado
+*/
+void imprimeSopaLetras(int numeroFilas, int numeroColumnas, char SopaLetras[numeroFilas][numeroColumnas])
+{
+    int i,j;
+    for(i=0; i<numeroFilas; i++)
+    {
+        printf("\n");
+        for(j=0; j<numeroColumnas; j++)
+        {
+            printf("%c\t", SopaLetras[i][j]);
+        }
+    } 
+    //apagar
+    int a;
+    scanf("%d",&a);
+}
 
+/*
+Representamos el tamaño del universo,
+jugar : void
+Retorno representa como el el programa fue finalizado
+*/
+void buscarPalabras(int tamanioUniverso, int numeroFilas, int numeroColumnas, char Universo[tamanioUniverso][MAXPALABRA], 
+                    char SopaLetras[numeroFilas][numeroColumnas])
+{
+    int i,j,k;
+    char palabra[MAXPALABRA];
+    
+    for(k=0; k<tamanioUniverso;k++)//para las palabras del universo
+    {
+        strcpy(palabra, Universo[k]);
+        for(i=0; i<numeroFilas; i++)
+        {
+            for(j=0; j<numeroColumnas; j++)
+            {
+                
+                printf("%s",palabra);
+                
+            }
+        }
+    }
+    //apagar
+    int a;
+    scanf("%d",&a);
+}
 
 
 
--- Trabajo Practico 2/SopaDeLetras/SopaDeLetras.h
+++ Trabajo Practico 2/SopaDeLetras/SopaDeLetras.h
@@ -4,7 +4,14 @@
 #include <assert.h>
 #include <string.h>
 
+//Definiciones 
+#define MAXPALABRA 100
+#define PRESENTE 1
+#define NOPRESENTE 0
+
 //Prototipos de las funciones
 int main();
 void jugar();
+void buscarPalabras(int tamanioUniverso, int numeroFilas, int numeroColumnas, char Universo[tamanioUniverso][MAXPALABRA], char SopaLetras[numeroFilas][numeroColumnas]);
+void imprimeSopaLetras(int numeroFilas, int numeroColumnas, char SopaLetras[numeroFilas][numeroColumnas]);
 //void testearFunciones();
