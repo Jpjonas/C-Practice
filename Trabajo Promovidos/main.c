da6b98d8004e11654ffe97bc92e5a41708fac7bf
Jonas Pollnow <jpjonas35@gmail.com>
2018-02-27 08:10:29 -0300

    distribuicion de las cartas

--- Trabajo Promovidos/main.c
+++ Trabajo Promovidos/main.c
@@ -4,30 +4,89 @@ int main(){
   printf("%s\n","Inicio del juego");
 
   //Cantidad de jugadores
-  int nrJugadores,i=0;
-  do{
-    printf("%s\n","Cuantos jugadores?");
-    scanf("%d\n", &nrJugadores);
-  }while((nrJugadores<3) && (nrJugadores>6));
+  int nrJugadores;;
+  printf("%s\n","Cuantos jugadores?(3-6)");
+  scanf("%d\n", &nrJugadores);
 
-  //Cartas al azar
+  //lista de las cartas sacadas
+  int listaCartas[21];
+  //Cartas al azar para las soluciones
   srand(time(NULL));
   //Solución sospechosos
   int solSospechoso = rand()%6;
+  listaCartas[0]=solSospechoso;
   //Solución habitación
-  int solHabitacion = rand()%9;
+  int solHabitacion = rand()%9+5;
+  listaCartas[1]=solHabitacion;
   //Solucción arma
-  int solArma = rand()%6;
+  int solArma = rand()%6+14;
+  listaCartas[2]=solArma;
+  printf("%s\n", "Tres cartas separadas como soluciones");
 
-  //alocación vector de nrJugadores
-  int **jugadores;
-  jugadores = (int **) malloc(nrJugadores * sizeof(int  *));
-
-  for(i=0;i<3;i++){
-    jugadores[i] = (int *) malloc()
+  //alocación del vector de jugadores
+  struct jugador listaJugadores[nrJugadores];
+  //inicialización de los datos
+  for(int jugador=0;jugador<nrJugadores;jugador++){
+    printf("%s\n","Cual es su nombre?");
+    scanf("%s\n", listaJugadores[jugador].nome);
+    listaJugadores[jugador].habitacion = Cocina;
+    listaJugadores[jugador].cartas = (int *) malloc(sizeof(int));
+    listaJugadores[jugador].perdio = NO;
   }
+  printf("%s\n", "Jugadores registrados");
 
+  //distribucion de las cartas
+  int proxJugador=0, proxCarta=3, contRondas=1;
+  for(int carta=0;carta<18;carta++){//Loop por todas las cartas
+    for(int cartaSol=0;cartaSol<3;cartaSol++){//Loop por las cartas ya sacadas
+      if(carta=!listaCartas[cartaSol]){//Verifica si la carta actual ya fue sacada
+        listaJugadores[proxJugador].cartas = (int *) realloc(listaJugadores[proxJugador].cartas, contRondas * sizeof(int));
+        listaJugadores[proxJugador].cartas[proxCarta] = carta;
+        proxJugador++;
+        proxCarta++;
+        if(proxJugador >= nrJugadores){
+          proxJugador = 0;
+          contRondas++;
+        }
+      }
+    }
+  }
+  printf("%s\n", "Cartas separadas");
 
+  //Jugadas
+  for(int jugador=0;jugador<nrJugadores;jugador++){
+      printf("%s %s\n", listaJugadores[jugador].nome, " queres hacer una acusación?(1-Si/0-No)");
+      int respuesta;
+      scanf("%d\n", &respuesta);
+      if(respuesta == 1){
+        if(hacerAcusacion(solSospechoso,solHabitacion,solArma,listaJugadores[jugador].habitacion) == 1){
+          printf("%s %s\n", listaJugadores[jugador].nome, " es el ganador");
+        }else{
+          printf("%s\n", "No es la respuesta");
+          listaJugadores[jugador].perdio = SI;
+        }
+      }
+      if(respuesta == 0){
 
+      }
+  }
+  return 0;
+}
+
+int hacerAcusacion(int solSospechoso, int solHabitacion, int solArma, int habitacionJugador){
+  int sospechoso, habitacion, arma;
+  printf("%s\n", "Quién es el sospechoso?");
+  printf("%s\n", "lista suspeitos");
+  scanf("%d\n", &sospechoso);
+  printf("%s\n", "Cual es la habitacion?");
+  printf("%s\n", "Lista habitaciones");
+  scanf("%d\n", &habitacion);
+  printf("%s\n", "Cual es la arma?");
+  printf("%s\n", "Lista armas");
+  scanf("%d\n", &arma);
+
+  if((sospechoso == solSospechoso) && (habitacion == solHabitacion) && (arma == solArma) && (habitacion == habitacionJugador)){
+    return 1;
+  }
   return 0;
 }
--- Trabajo Promovidos/main.h
+++ Trabajo Promovidos/main.h
@@ -13,42 +13,47 @@
 #include <string.h>
 #include <time.h>
 
-typedef struct direc {     
-  char nombre[30];     
-  char calle[40];     
-  char ciudad[20];     
-  char estado[3];     
-  unsigned int codigo;
-} sdirec;
+//STRUCTS
+struct jugador{
+  char nome[30];
+  int habitacion;
+  int *cartas;
+  int perdio; //0 -> continua en el juego. 1 -> perdio el juego
+};
 
 //DEFINICIONES
 #define SOSPECHOSOS 6;
 #define HABITACIONES 9;
 #define ARMAS 6;
-
-//SOSPECHOSOS
+#define SI 1;
+#define NO 0;
+#define NOGANADOR 0;
+#define GANADOR 1;
+//Sospechosos
 #define SenoritaEscarlata 0;
 #define CoronelMostaza 1;
 #define ProfesorMoradillo 2;
 #define SenoraAzulino 3;
 #define SenorVerdi 4;
 #define SenoraBlanco 5;
+//Habitaciones
+#define Cocina 6;
+#define Patio 7;
+#define Spa 8;
+#define Teatro 9;
+#define Salon 10;
+#define Observatorio 11;
+#define Vestibulo 12;
+#define HabitacionHuespedes 13;
+#define Comedor 14;
+//Armas
+#define Revolver 15;
+#define Cuchillo 16;
+#define Lazo 17;
+#define Candelabro 18;
+#define LlaveInglesa 19;
+#define TuboPlomo 20;
 
-//HABITACIONES
-#define Cocina 0;
-#define Patio 1;
-#define Spa 2;
-#define Teatro 3;
-#define Salon 4;
-#define Observatorio 5;
-#define Vestibulo 6;
-#define HabitacionHuespedes 7;
-#define Comedor 8;
+//PROTOTIPOS Y DOCUMENTACIÓN DE LAS FUNCIONES
 
-//ARMAS
-#define Revolver 0;
-#define Cuchillo 1;
-#define Lazo 2;
-#define Candelabro 3;
-#define LlaveInglesa 4;
-#define TuboPlomo 5;
+int hacerAcusacion(int solSospechoso, int solHabitacion, int solArma, int ubicacionJugador);
